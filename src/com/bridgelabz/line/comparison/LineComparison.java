e7d4f77aeb1ef902d630ed88bada6c5f273ebb04
tejuypatil <tejuypatil@gmail.com>
2022-07-16 00:50:11 +0530

    Added Welcome msg

---
+++ src/com/bridgelabz/line/comparison/LineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.line.comparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+
+}
